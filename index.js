b6564199b34ad57cb20b6edbf942a96c43270e58
Jarmo Toivanen <toivanen@protonmail.com>
2023-09-14 15:50:49 +0300

    staticfiles server

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const cors = require('cors');
 const app = express();
 
 app.use(express.json());
+
+app.use(express.static('dist'));
 app.use(cors());
 
 morgan.token('body', function (req, res) {
@@ -12,6 +14,7 @@ morgan.token('body', function (req, res) {
 
 const format =
   ':method :url :status :res[content-length] - :response-time ms :body';
+
 app.use(morgan(format));
 
 const unknownEndpoint = (request, response) => {
