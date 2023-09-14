f2db3fb2ee8657b3d9a58361322409f274a0d621
Jarmo Toivanen <toivanen@protonmail.com>
2023-09-14 12:42:03 +0300

    cors

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const morgan = require('morgan');
+const cors = require('cors');
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
 morgan.token('body', function (req, res) {
   return JSON.stringify(req.body);
@@ -115,7 +117,8 @@ app.get('/info', (req, res) => {
 
 app.use(unknownEndpoint);
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
+
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
