3408b491eeeceefb29ea4f5860678a0be7fa9378
Jarmo Toivanen <toivanen@protonmail.com>
2023-09-18 11:20:54 +0300

    Casterror and 500 added

--- utils/errorHandler.js
+++ utils/errorHandler.js
@@ -1,4 +1,24 @@
+const AppError = require('../utils/AppError');
+
+module.exports = (err, req, res, next) => {
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || 'error';
+
+  if (process.env.NODE_ENV === 'development') {
+    sendDevError(err, res);
+  } else if (process.env.NODE_ENV === 'production') {
+    sendProdError(err, res);
+  }
+};
+
 const sendDevError = (err, res) => {
+  if (err.name === 'CastError') {
+    const message = 'Invalid ID format';
+    err = new AppError(message, 400);
+  } else if (!err.statusCode) {
+    err = new AppError('Database connection error', 500);
+  }
+
   res.status(err.statusCode).json({
     status: err.status,
     error: err,
@@ -19,16 +39,5 @@ const sendProdError = (err, res) => {
       status: 'error',
       message: 'Something went wrong!',
     });
-  }
-};
-
-module.exports = (err, req, res, next) => {
-  err.statusCode = err.statusCode || 500;
-  err.status = err.status || 'error';
-
-  if (process.env.NODE_ENV === 'development') {
-    sendDevError(err, res);
-  } else if (process.env.NODE_ENV === 'production') {
-    sendProdError(err, res);
   }
 };
