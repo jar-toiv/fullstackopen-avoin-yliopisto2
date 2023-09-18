500cf2a4f7beb41a92ca894414666e141a25c768
Jarmo Toivanen <toivanen@protonmail.com>
2023-09-18 14:50:51 +0300

    osa3 Phonebook exercise 3.20 done

--- models/personModel.js
+++ models/personModel.js
@@ -4,12 +4,20 @@ const personSchema = new mongoose.Schema(
   {
     name: {
       type: String,
-      minLength: 3,
+      minLength: [3, 'Name must be be at least 3 characters long.'],
       required: [true, 'A person must have a name.'],
     },
     number: {
       type: String,
       required: [true, 'A person must have a number '],
+      minlength: [8, 'Phone number must be at least 8 characters long.'],
+      validate: {
+        validator: function (v) {
+          return /^(\d{2,3})-(\d{5,8})$/.test(v);
+        },
+        message: (props) =>
+          `${props.value} is not a valid phone number! A valid phone number should be in the format: 09-1234556 or 040-22334455`,
+      },
     },
   },
   { collection: 'person' }
