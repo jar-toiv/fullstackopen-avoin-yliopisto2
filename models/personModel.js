a16191da94b1766635963be2d663d69636bbdd70
Jarmo Toivanen <toivanen@protonmail.com>
2023-09-18 10:11:32 +0300

    feat: transformer and NUM can be STR

--- models/personModel.js
+++ models/personModel.js
@@ -3,9 +3,19 @@ const mongoose = require('mongoose');
 const personSchema = new mongoose.Schema(
   {
     name: String,
-    number: Number,
+    number: String,
   },
   { collection: 'person' }
 );
+
+//¤ https://mongoosejs.com/docs/api/document.html#transform
+
+personSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString();
+    delete returnedObject._id;
+    delete returnedObject.__v;
+  },
+});
 
 module.exports = mongoose.model('Person', personSchema);
